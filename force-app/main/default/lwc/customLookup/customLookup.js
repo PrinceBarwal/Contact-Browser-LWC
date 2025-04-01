2d4b6a1103052ba5f422589ae421da581baa0b50
Prince saini <prince.saini@sltech.local>
2025-04-01 12:05:19 +0530

    Adding Custom Event on Custom Lookup Component

---
+++ force-app/main/default/lwc/customLookup/customLookup.js
@@ -0,0 +1,72 @@
+import { LightningElement, wire, api } from 'lwc';
+import customLookupController from '@salesforce/apex/customLookupController.getsObjectRecords';
+const DELAY = 300;
+export default class CustomLookup extends LightningElement {
+
+    @api apiName = "Account";
+    searchValue;
+    @api objectLabel  = 'Account';
+    @api iconName = 'standard:account';
+    delayTimeOut;
+    selectedRecord = {
+        selectedId : "",
+        selectedName : ""
+    }
+    displayOptions = false;
+
+    @wire(customLookupController, 
+        {
+            objectApiName : '$apiName',
+            searchKey : '$searchValue'
+        }
+    )outputs
+
+    // get showOutput(){
+    //     return this.outputs.data ? true : false;
+    // }
+
+    changeHandler(event){
+        window.clearTimeout(this.delayTimeOut);
+        let enteredValue = event.target.value;
+        this.delayTimeOut = setTimeout(() => {
+            this.searchValue = enteredValue;
+            this.displayOptions = true;
+        } , DELAY);
+    }
+
+    clickHandler(event){
+        let selectedId = event.currentTarget.dataset.item;
+        console.log('select Id',selectedId);
+        let outputRecord = this.outputs.data.find(
+            (currItem) => currItem.Id === selectedId
+        );
+        this.selectedRecord = {
+            selectedId : outputRecord.Id,
+            selectedName : outputRecord.Name
+        };
+        this.sendSelection();
+        this.displayOptions = false;
+    }   
+
+    get isRecordSelected(){
+        return this.selectedRecord.selectedId === '' ? false : true;
+    }
+
+    removalSelectionHandler(event){
+        console.log('event called');
+        this.selectedRecord = {
+            selectedId : "",
+            selectedName : ""
+        };
+        this.sendSelection();
+        this.displayOptions = false;
+    }
+
+    sendSelection(){
+        let mySelectionEvent = new CustomEvent("selectedrec", {
+            detail : this.selectedRecord.selectedId
+        });
+
+        this.dispatchEvent(mySelectionEvent);
+    }
+}
